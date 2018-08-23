ebbf114076526189e5048da73a36f1d0c230907f
k-kohey <kawagutsuchi@gmail.com>
2018-08-23 13:17:38 +0900

    fist init

---
+++ MotherWeatherNewsIos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotherWeatherNewsIos
+//
+//  Created by kawaguchi kohei on 2018/08/23.
+//  Copyright © 2018年 kawaguchi kohei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MotherWeatherNewsIos' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MotherWeatherNewsIos
+
+end
